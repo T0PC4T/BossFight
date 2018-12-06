a6401f8629fa452f37699732f0087dcfa160fa71
Thomas Clovis <tom@itproz.co.uk>
2018-12-06 11:09:11 +0000

    elements.go
    test

--- brawl/elements.go
+++ brawl/elements.go
@@ -7,12 +7,11 @@ import "github.com/hajimehoshi/ebiten"
 type element struct {
 	ID     int
 	s      *sprite
-	l      *level
 	status string
 	x, y   float64
-	vx, vy float64
-	w, h   float64
+	w, h   int
 
+	store      map[string]interface{}
 	components []component
 }
 
@@ -24,20 +23,10 @@ func (e *element) setPos(x, y float64) error {
 	e.x, e.y = x, y
 	return nil
 }
-func (e *element) getTopLeft() (float64, float64) {
-	return e.x - e.w/2, e.y - e.h/2
-}
-func (e *element) getVel() (float64, float64) {
-	return e.vx, e.vy
-}
-func (e *element) setVel(vx, vy float64) error {
-	e.vx, e.vy = vx, vy
-	return nil
-}
-func (e *element) getDim() (float64, float64) {
+func (e *element) getDim() (int, int) {
 	return e.w, e.h
 }
-func (e *element) setDim(w, h float64) error {
+func (e *element) setDim(w, h int) error {
 	e.w, e.h = w, h
 	return nil
 }
@@ -56,11 +45,10 @@ func (e *element) update() error {
 }
 
 func (e *element) draw(screen *ebiten.Image) error {
-	canvas, _ := ebiten.NewImage(int(e.w), int(e.h), ebiten.FilterDefault)
+	canvas, _ := ebiten.NewImage(e.w, e.h, ebiten.FilterDefault)
 	e.s.draw(canvas)
 
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(-e.w/2, -e.h/2)
 	op.GeoM.Translate(e.x, e.y)
 	screen.DrawImage(canvas, op)
 	return nil
@@ -70,17 +58,18 @@ func (e *element) addComponent(c component) {
 	e.components = append(e.components, c)
 }
 
-// func (e *element) removeComponent(name string) bool {
-// 	for i, c := range e.components {
-// 		if c.getName() == name {
+func (e *element) removeComponent(name string) bool {
+	for i, c := range e.components {
+		if c.getName() == name {
 
-// 		}
-// 	}
-// 	if index < len(e.components) {
-// 		e.components[ID] = e.components[len(e.components)-1]
-// 		e.components = e.components[:len(e.components)-1]
-// 	} else {
-// 		return false
-// 	}
-// 	return true
-// }
+		}
+	}
+
+	if index < len(e.components) {
+		e.components[ID] = e.components[len(e.components)-1]
+		e.components = e.components[:len(e.components)-1]
+	} else {
+		return false
+	}
+	return true
+}
